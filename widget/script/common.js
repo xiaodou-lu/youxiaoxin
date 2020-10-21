5d767f3ce69b8b824d8e23bdee9c60ca92a233fd
luwenya <2055257783@qq.com>
2020-10-21 17:00:37 +0800

    修改登录及注册所用token

--- widget/script/common.js
+++ widget/script/common.js
@@ -1,3 +1,6 @@
-var baseUrl = 'https://api.ailiaolive.cn/api/live/v1/'; // 接口域名地址
-var qiniuBaseUrl = 'http://upload.qiniup.com/';
-var qiniuImageUrl = 'http://qiniu.ailiaolive.cn/'; // 七牛图片地址
+var baseUrl = 'http://api.jixiangguo.cc/api/live/v1/'; // 接口域名地址
+var qiniuBaseUrl = 'http://upload-z2.qiniup.com/';
+var qiniuImageUrl = 'http://qiniu.jixiangguo.cc/'; // 七牛图片地址
+// var baseUrl = 'https://api.ailiaolive.cn/api/live/v1/'; // 接口域名地址
+// var qiniuBaseUrl = 'http://upload.qiniup.com/';
+// var qiniuImageUrl = 'http://qiniu.ailiaolive.cn/'; // 七牛图片地址
